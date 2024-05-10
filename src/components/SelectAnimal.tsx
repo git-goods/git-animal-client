075f22aac202fadbe6b569c825f5d0108cf2c9fc
sumi-0011 <selina2000@naver.com>
2024-05-11 01:18:51 +0900

    feat: 외부 이미지 링크 연결

--- src/components/SelectAnimal.tsx
+++ src/components/SelectAnimal.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import styled from 'styled-components';
 
 import { convertPersonaTypeToImageName } from '@/utils/string';
+import { STATIC_IMAGE_URL } from '@/constants/outlink';
 
 interface Props {
   selected?: string;
