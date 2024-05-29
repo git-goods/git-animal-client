2301abe5fefff0dce25406c262356ecfd1c7574d
sumi-0011 <selina2000@naver.com>
2024-05-29 18:00:48 +0900

    refactor: product table view, logic 분리

--- src/pages/shop/HistoryTable.tsx
+++ src/pages/shop/HistoryTable.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-
 import { useMySellHistory } from '@/apis/auctions/useGetHistory';
+import { ACTION_BUTTON_OBJ } from '@/constants/action';
+import type { ProductHistoryType, ProductItemType } from '@/schema/action';
 
-import ShopTable from './ShopTable';
+import ShopTableBackground from './ShopTableBackground';
+import ShopTableRowView from './ShopTableRowView';
+
+const HISTORY_ACTION_OBJ = ACTION_BUTTON_OBJ['SELL_HISTORY'];
 
 function HistoryTable() {
   const { data } = useMySellHistory();
-  console.log('data: ', data);
 
-  return <ShopTable list={data?.products ?? []} />;
+  const onAction = (item: ProductItemType) => {
+    console.log('onAction: ', item);
+  };
+
+  const getHistoryActionLabel = (soldAt: string) => {
+    return soldAt?.slice(2, 10).replace(/-/g, '.');
+  };
+
+  return (
+    <ShopTableBackground>
+      {data?.products.map((product) => {
+        return (
+          <ShopTableRowView
+            key={product.id}
+            item={product}
+            onAction={onAction}
+            actionLabel={getHistoryActionLabel((product as ProductHistoryType)?.receipt.soldAt)}
+            actionColor={HISTORY_ACTION_OBJ.color}
+          />
+        );
+      })}
+    </ShopTableBackground>
+  );
 }
 
 export default HistoryTable;
--- src/pages/shop/ProductTable.tsx
+++ src/pages/shop/ProductTable.tsx
@@ -1,11 +1,32 @@
 import { useGetProductForProductList } from '@/apis/auctions/useGetProducts';
+import { ACTION_BUTTON_OBJ } from '@/constants/action';
+import type { ProductItemType } from '@/schema/action';
 
-import ShopTable from './ShopTable';
+import ShopTableBackground from './ShopTableBackground';
+import ShopTableRowView from './ShopTableRowView';
 
 function ProductTable() {
   const { data } = useGetProductForProductList();
 
-  return <ShopTable list={data?.products ?? []} />;
+  const onAction = (item: ProductItemType) => {
+    console.log('onAction: ', item);
+  };
+
+  return (
+    <ShopTableBackground>
+      {data?.products.map((product) => {
+        return (
+          <ShopTableRowView
+            key={product.id}
+            item={product}
+            onAction={onAction}
+            actionLabel={ACTION_BUTTON_OBJ[product.paymentState].label}
+            actionColor={ACTION_BUTTON_OBJ[product.paymentState].color}
+          />
+        );
+      })}
+    </ShopTableBackground>
+  );
 }
 
 export default ProductTable;
