892db03ad61d4bb75a9ecc86b143741195a6ba2d
sumi-0011 <selina2000@naver.com>
2024-06-12 12:29:09 +0900

    feat: meta tag 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,35 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'GitAnimals',
+  description: '깃허브 활동으로 펫을 키우세요!',
+  //   url: 'https://www.gitanimals.org/',
+  //   image: '/og-image.png',
+  authors: [
+    {
+      name: 'sumi-0011',
+      url: 'https://github.com/sumi-0011',
+    },
+    {
+      url: 'https://github.com/hyesungoh',
+      name: 'hyesungoh',
+    },
+  ],
+  generator: 'GitAnimals',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.gitanimals.org/',
+    images: [
+      {
+        url: '/og-image.png',
+      },
+    ],
+    title: 'GitAnimals',
+    description: '깃허브 활동으로 펫을 키우세요!',
+    siteName: 'GitAnimals',
+  },
+};
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
