9c15163802cb5d064e4b59285cd141abcb7a105b
sumi-0011 <selina2000@naver.com>
2024-06-14 16:38:23 +0900

    refactor: env 제거

--- apps/web/src/apis/index.ts
+++ apps/web/src/apis/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { setInterceptors } from './interceptor';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = 'https://api.gitanimals.org'
 
 const instance = setInterceptors(
   axios.create({
