eed4deca4273aeb47514d4253ee6daf794bd0292
hyesungoh <haesungoh414@gmail.com>
2024-06-15 17:58:36 +0900

    feature: set transpile packages and output

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  transpilePackages: ['@gitanimals/ui'],
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
