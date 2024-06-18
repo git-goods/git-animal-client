005f7b4863db4cff954a0ac1ada95a040a9ef35b
sumi-0011 <selina2000@naver.com>
2024-06-18 16:37:13 +0900

    refactor: select component folder move

--- apps/web/src/components/FeedBack/FeedBack.tsx
+++ apps/web/src/components/FeedBack/FeedBack.tsx
@@ -2,8 +2,9 @@
 
 import styled from 'styled-components';
 
+import Select from '@/components/Select';
+
 import { ISSUE_LABEL } from './FeedBack.constants';
-import Select from './Select';
 
 function FeedBack() {
   return (
