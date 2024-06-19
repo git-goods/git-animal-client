8d7b2c808d9444460e928f9cb8ceca68244c94cd
sumi.byun <selina2000@naver.com>
2024-06-19 15:31:58 +0900

    TextArea UI 컴포넌트 추가 (#61)
    * feat: textarea ui 추가
    
    * Update apps/web/src/components/TextArea.tsx
    
    Co-authored-by: hyesung oh <haesungoh414@gmail.com>
    
    * fix: 리뷰 반영
    
    * fix: maxLength 누락 수정
    
    ---------
    
    Co-authored-by: hyesung oh <haesungoh414@gmail.com>

---
+++ apps/web/src/components/TextArea.tsx
@@ -0,0 +1,107 @@
+import type { ChangeEvent, ComponentProps } from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+interface TextAreaProps extends ComponentProps<'textarea'> {
+  error?: boolean;
+}
+
+function TextArea({ maxLength = 300, ...props }: TextAreaProps) {
+  const [inputLen, setInputLen] = useState(String(props.value ?? '')?.length || 0);
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputLen(e.target.value.length);
+    props?.onChange?.(e);
+  };
+
+  return (
+    <Container $error={props.error}>
+      <TextAreaStyled {...props} onChange={onChange} maxLength={maxLength} />
+      <TextLen>
+        <strong>{inputLen}</strong>
+        <span> / {maxLength}</span>
+      </TextLen>
+    </Container>
+  );
+}
+
+export default TextArea;
+
+const TextAreaStyled = styled.textarea`
+  background: transparent;
+  border: none;
+
+  color: #000000bf;
+  padding: 16px 20px;
+
+  /* glyph16 regular */
+  font-family: 'Product Sans';
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%; /* 24px */
+  letter-spacing: -0.3px;
+
+  border-radius: 8px;
+  outline: none;
+  width: 100%;
+  resize: none;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.5);
+    /* glyph16 regular */
+    font-family: 'Product Sans';
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 150%; /* 24px */
+    letter-spacing: -0.3px;
+  }
+`;
+
+const TextLen = styled.div`
+  position: absolute;
+  bottom: 8px;
+  color: rgba(0, 0, 0, 0.5);
+  right: 20px;
+  font-family: 'Product Sans';
+  font-size: 12px;
+  font-weight: 400;
+  width: fit-content;
+
+  strong {
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.75);
+  }
+`;
+
+const Container = styled.div<{
+  $error?: boolean;
+}>`
+  position: relative;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  padding-bottom: 26px;
+
+  &:focus-within {
+    border: 1px solid #00894d;
+
+    ${TextLen} {
+      strong {
+        color: #00894d;
+      }
+    }
+  }
+
+  ${({ $error }) =>
+    $error &&
+    `
+    ${TextAreaStyled} {
+      border: 1px solid #FF6B56;
+    }
+
+    ${TextLen} {
+      strong {
+        color: #FF6B56;
+      }
+    }
+  `}
+`;
