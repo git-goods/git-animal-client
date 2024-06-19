4950affca6584a08f015ac0cda2f335f8649e440
sumi-0011 <selina2000@naver.com>
2024-06-19 23:24:41 +0900

    feat: mobile ui design 적용

--- apps/web/src/components/FeedBack/FeedBack.tsx
+++ apps/web/src/components/FeedBack/FeedBack.tsx
@@ -44,8 +44,8 @@ function FeedBack() {
         </Select>
         <Input placeholder="Type issue title..." />
         <TextArea placeholder="Please feel free to leave any good points for improvement..." />
-        <ButtonStyled>Send</ButtonStyled>
       </Form>
+      <ButtonStyled>Send</ButtonStyled>
     </Container>
   );
 }
@@ -54,7 +54,12 @@ export default FeedBack;
 
 const ButtonStyled = styled(Button)`
   width: fit-content;
-  margin: 0 auto;
+  margin: 24px auto;
+
+  @media (max-width: 768px) {
+    width: calc(100% - 32px);
+    margin: 16px;
+  }
 `;
 
 const Container = styled.div`
@@ -63,11 +68,17 @@ const Container = styled.div`
   right: 24px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
   width: 406px;
 
   box-shadow: 0px 4px 24px 0px rgba(0, 0, 0, 0.25);
   background-color: #fff;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    bottom: 0;
+    right: 0;
+    left: 0;
+  }
 `;
 
 const IssueOptionColor = styled.div`
@@ -112,6 +123,9 @@ const Heading = styled.section`
     line-height: 130%; /* 18.2px */
     letter-spacing: -0.3px;
   }
+  @media (max-width: 768px) {
+    padding: 16px;
+  }
 `;
 
 const Form = styled.section`
@@ -125,11 +139,11 @@ function CloseIcon() {
   return (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M3.68076 2.62009C3.38788 2.32718 2.91301 2.32716 2.6201 2.62004C2.3272 2.91291 2.32717 3.38779 2.62005 3.6807L8.93938 10.0006L2.62004 16.3206C2.32716 16.6135 2.32718 17.0884 2.62009 17.3812C2.91299 17.6741 3.38787 17.6741 3.68075 17.3812L9.99999 11.0613L16.3192 17.3812C16.6121 17.6741 17.087 17.6741 17.3799 17.3812C17.6728 17.0884 17.6728 16.6135 17.3799 16.3206L11.0606 10.0006L17.3799 3.6807C17.6728 3.38779 17.6728 2.91291 17.3799 2.62004C17.087 2.32716 16.6121 2.32718 16.3192 2.62009L9.99999 8.93992L3.68076 2.62009Z"
         fill="black"
-        fill-opacity="0.5"
+        fillOpacity="0.5"
       />
     </svg>
   );
