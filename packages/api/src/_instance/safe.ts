07347ccabfaa3e42427344529acd5101f7af763e
hyesungoh <haesungoh414@gmail.com>
2024-06-21 11:26:30 +0900

    refactor: using safe factory

--- packages/api/src/_instance/safe.ts
+++ packages/api/src/_instance/safe.ts
@@ -2,10 +2,13 @@ import { ZodType, z } from 'zod';
 import { del, get, patch, post, put } from './default';
 import { CustomException } from '@gitanimals/exception';
 
-export const safeGet =
+type Method = typeof get | typeof post | typeof put | typeof patch | typeof del;
+
+const safeFactory =
+  <A extends Parameters<Method>>(method: (...args: A) => ReturnType<Method>) =>
   <Z extends ZodType>(zodSchema: Z) =>
-  async (...args: Parameters<typeof get>): Promise<z.infer<Z>> => {
-    const response = await get(...args);
+  async (...args: A): Promise<z.infer<Z>> => {
+    const response = await method(...args);
 
     const parsed = zodSchema.safeParse(response);
     if (parsed.error) throw new CustomException('API_TYPE_NOT_MATCH');
@@ -13,16 +16,8 @@ export const safeGet =
     return response;
   };
 
-// type Method = typeof get | typeof post | typeof put | typeof patch | typeof del;
-
-// const safeFactory =
-//   <M extends Method>(method: M) =>
-//   <Z extends ZodType>(zodSchema: Z) =>
-//   async (...args: Parameters<M>): Promise<z.infer<Z>> => {
-//     const response = await method(...args);
-
-//     const parsed = zodSchema.safeParse(response);
-//     if (parsed.error) throw new CustomException('API_TYPE_NOT_MATCH');
-
-//     return response;
-//   };
+export const safeGet = safeFactory(get);
+export const safePost = safeFactory(post);
+export const safePut = safeFactory(put);
+export const safePatch = safeFactory(patch);
+export const safeDel = safeFactory(del);
