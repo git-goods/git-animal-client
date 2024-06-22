7a04ea70410d603e8b5f59bcb62d196362b9e131
hyesungoh <haesungoh414@gmail.com>
2024-06-22 20:20:08 +0900

    feat: set @gitanimals/ui-panda

---
+++ packages/ui/panda/panda.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from '@pandacss/dev';
+import { tokens, semanticTokens } from './src/theme';
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ['./src/**/*.{ts,tsx}'],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      tokens,
+      semanticTokens,
+    },
+  },
+
+  outExtension: 'js',
+  jsxFramework: 'react',
+
+  // The output directory for your css system
+  outdir: 'styled-system',
+
+  // delete default presets
+  presets: [],
+});
