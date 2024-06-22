be8104c1c3d9e46c861e6662bb4121f0d87422e7
hyesungoh <haesungoh414@gmail.com>
2024-06-22 18:54:41 +0900

    feat: @gitanimals/token

---
+++ packages/ui/token/src/color/black-and-white.ts
@@ -0,0 +1,18 @@
+export const BLACK_AND_WHITE = {
+  black: '#000000',
+  black_100: 'rgba(0, 0, 0, 1)',
+  black_90: 'rgba(0, 0, 0, 0.9)',
+  black_75: 'rgba(0, 0, 0, 0.75)',
+  black_50: 'rgba(0, 0, 0, 0.5)',
+  black_25: 'rgba(0, 0, 0, 0.25)',
+  black_10: 'rgba(0, 0, 0, 0.1)',
+  black_5: 'rgba(0, 0, 0, 0.05)',
+  white: '#FFFFFF',
+  white_100: 'rgba(255, 255, 255, 1)',
+  white_90: 'rgba(255, 255, 255, 0.9)',
+  white_75: 'rgba(255, 255, 255, 0.75)',
+  white_50: 'rgba(255, 255, 255, 0.5)',
+  white_25: 'rgba(255, 255, 255, 0.25)',
+  white_10: 'rgba(255, 255, 255, 0.1)',
+  white_5: 'rgba(255, 255, 255, 0.05)',
+} as const;
---
+++ packages/ui/token/src/color/index.ts
@@ -0,0 +1,4 @@
+import { BLACK_AND_WHITE } from './black-and-white';
+import { BRAND_COLOR } from './brand';
+
+export const COLORS = { ...BLACK_AND_WHITE, ...BRAND_COLOR };
