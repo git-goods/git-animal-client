df923e30ca4aac06c6a1e9345642012bf7bd9633
sumi-0011 <selina2000@naver.com>
2024-06-30 18:08:58 +0900

    feat: landing layout 제거

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-
-import Header from '@/components/Layout/Header';
-import Layout from '@/components/Layout/Layout';
-
-import Welcome from './Welcome';
-
 export default function Home() {
-  return (
-    <Layout>
-      <Header />
-      <Welcome />
-    </Layout>
-  );
+  return <div></div>;
 }
