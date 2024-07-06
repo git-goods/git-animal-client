e8af1c716459fc6060dda3e2c6105030436616b7
sumi-0011 <selina2000@naver.com>
2024-07-06 23:49:04 +0900

    feat: avaliable bg 추가

--- apps/web/src/app/landing/AvailablePetSection/AvailablePetSection.tsx
+++ apps/web/src/app/landing/AvailablePetSection/AvailablePetSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import { Button } from '@gitanimals/ui-panda';
 
 import * as styles from './AvailablePetSection.style';
@@ -10,28 +11,38 @@ const REGISTERED_PETS = 3400;
 function AvailablePetSection() {
   return (
     <section className={styles.container}>
-      <h2 className={styles.heading}>50+ Available Pets</h2>
-      <div className={styles.infoContainer}>
-        <div className={styles.infoItem}>
-          <p>{TOTAL_USERS}+</p>
-          <p>Total Users</p>
+      <picture className={styles.bgPictureTop}>
+        <source srcSet="/main/section2_bg-top.webp" type="image/webp" />
+        <Image src="/main/section2_bg-top.png" alt="Available Pets" width={1920} height={382} />
+      </picture>
+      <picture className={styles.bgPictureBottom}>
+        <source srcSet="/main/section2_bg-bottom.webp" type="image/webp" />
+        <Image src="/main/section2_bg-bottom.png" alt="Available Pets" width={1920} height={382} />
+      </picture>
+      <div className={styles.content}>
+        <h2 className={styles.heading}>50+ Available Pets</h2>
+        <div className={styles.infoContainer}>
+          <div className={styles.infoItem}>
+            <p>{TOTAL_USERS}+</p>
+            <p>Total Users</p>
+          </div>
+          <div className={styles.infoItem}>
+            <p>{TOTAL_ADOPTED_PETS}+</p>
+            <p>Total Adopted Pets</p>
+          </div>
+          <div className={styles.infoItem}>
+            <p>{REGISTERED_PETS}+</p>
+            <p>Registered Pets for Adoption</p>
+          </div>
         </div>
-        <div className={styles.infoItem}>
-          <p>{TOTAL_ADOPTED_PETS}+</p>
-          <p>Total Adopted Pets</p>
+        <div className={styles.buttonWrapper}>
+          <Button size="m" className="mobile">
+            Show More Pets
+          </Button>
+          <Button size="l" className="desktop">
+            Show More Pets
+          </Button>
         </div>
-        <div className={styles.infoItem}>
-          <p>{REGISTERED_PETS}+</p>
-          <p>Registered Pets for Adoption</p>
-        </div>
-      </div>
-      <div className={styles.buttonWrapper}>
-        <Button size="m" className="mobile">
-          Show More Pets
-        </Button>
-        <Button size="l" className="desktop">
-          Show More Pets
-        </Button>
       </div>
     </section>
   );
