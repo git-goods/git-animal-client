befed05a9c56a1596711f2b8dbf2ff3cdb18c35a
sumi-0011 <selina2000@naver.com>
2024-07-14 00:31:01 +0900

    feat: 허전하니까 오른쪽에 프로필 추가

--- apps/admin/app/components/layout/Header/Header.tsx
+++ apps/admin/app/components/layout/Header/Header.tsx
@@ -1,10 +1,17 @@
 import { css } from '_panda/css';
 import Nav from './Nav';
+import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar';
 
 function Header() {
   return (
     <div className={containerStyle}>
       <Nav />
+      <div className={avatarStyle}>
+        <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/49177223?v=4" alt="@nanopx" />
+          <AvatarFallback>NP</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   );
 }
@@ -17,7 +24,25 @@ const containerStyle = css({
   shadow: 'md',
   zIndex: 'sticky',
   px: 4,
+  py: 3,
 
   display: 'flex',
   alignItems: 'center',
+  justifyContent: 'space-between',
+});
+
+const avatarStyle = css({
+  overflow: 'hidden',
+  borderRadius: '50%',
+  height: '100%',
+  background: 'muted',
+  '& span': {
+    height: '100%',
+    display: 'block',
+    '& img': {
+      borderRadius: '50%',
+      height: '100%',
+      width: 'auto',
+    },
+  },
 });
