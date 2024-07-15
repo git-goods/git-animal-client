76e4a58869e853fdb45fe22cfdeb0c33d988fc4f
sumi-0011 <selina2000@naver.com>
2024-07-15 19:03:14 +0900

    feat: buy product api에서 token 받도록 설정

--- packages/api/src/auction/buyProduct.ts
+++ packages/api/src/auction/buyProduct.ts
@@ -6,10 +6,19 @@ import { safePost } from '../_instance/safe';
 
 export type BuyProductRequest = {
   productId: string;
+  token: string;
 };
 
 export type BuyProductResponse = z.infer<typeof HistoryProductSchema>;
 
 export const buyProduct = async (request: BuyProductRequest): Promise<BuyProductResponse> => {
-  return await safePost(HistoryProductSchema)(`/auctions/products/${request.productId}`);
+  return await safePost(HistoryProductSchema)(
+    `/auctions/products/${request.productId}`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${request.token}`,
+      },
+    },
+  );
 };
