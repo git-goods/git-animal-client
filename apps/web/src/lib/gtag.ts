fb210db6c6ae1b841f15b225f6b897b356ea1da5
sumi-0011 <selina2000@naver.com>
2024-07-15 23:48:42 +0900

    fix: 얼리 리턴으로 변경

--- apps/web/src/lib/gtag.ts
+++ apps/web/src/lib/gtag.ts
@@ -3,11 +3,11 @@ const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ?? 'G-RNEDVMFT5X
 export const pageview = (url: URL) => {
   if (process.env.NODE_ENV !== 'production') return;
 
-  if (typeof window !== 'undefined') {
-    window.gtag('config', GA_TRACKING_ID, {
-      page_path: url,
-    });
-  }
+  if (typeof window === 'undefined') return;
+
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
 };
 
 interface GTagEvent {
@@ -19,14 +19,13 @@ interface GTagEvent {
 
 export const event = ({ action, category, label, value }: GTagEvent) => {
   if (process.env.NODE_ENV !== 'production') return;
+  if (typeof window === 'undefined') return;
 
-  if (typeof window !== 'undefined') {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    });
-  }
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
 };
 
 export const recordEvent = event;
