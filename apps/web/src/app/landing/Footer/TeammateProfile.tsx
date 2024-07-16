8f5c77e80b82ae27c6bb28e2e6bfc3ff584352fc
hyesungoh <haesungoh414@gmail.com>
2024-07-16 20:52:12 +0900

    feat: teams icon with appending behance icon

--- apps/web/src/app/landing/Footer/TeammateProfile.tsx
+++ apps/web/src/app/landing/Footer/TeammateProfile.tsx
@@ -1,5 +1,6 @@
-import type { FC } from 'react';
+import type { FC, ReactElement } from 'react';
 import Image from 'next/image';
+import { BehanceIcon, GithubIcon } from '@gitanimals/ui-icon';
 
 import * as styles from './TeammateProfile.style';
 
@@ -16,12 +17,19 @@ interface Props {
 }
 
 export const TeammateProfile: FC<Props> = ({ nickname, image, urlType, url, role }) => {
+  const ICON_MAP: Record<Props['urlType'], ReactElement> = {
+    github: <GithubIcon />,
+    behance: <BehanceIcon />,
+  };
+
   return (
     <div className={styles.wrapperCss}>
       <Image className={styles.imageCss} src={image.src} width={image.width} height={image.height} alt={nickname} />
       <span className={styles.nicknameWrapperCss}>
         <span>{nickname}</span>
-        {/* TODO: icon */}
+        <a target="_blank" href={url}>
+          {ICON_MAP[urlType]}
+        </a>
       </span>
       <span className={styles.roleCss}>{role}</span>
     </div>
