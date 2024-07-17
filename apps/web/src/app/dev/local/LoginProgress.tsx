35f31f837a01719ba8631dcc10b1e4df1075f3b6
sumi-0011 <selina2000@naver.com>
2024-07-17 15:59:35 +0900

    fix: build error fix (강제로 마운트 뒤에 실행)

---
+++ apps/web/src/app/dev/local/LoginProgress.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+import { useLogin } from '@/store/user';
+
+function DevLocalLogin() {
+  const searchParams = useSearchParams();
+  const token = searchParams.get('token');
+
+  const router = useRouter();
+  const { login } = useLogin();
+
+  const onLogin = async (token: string) => {
+    try {
+      await login(token);
+      router.replace('/mypage');
+    } catch (error) {
+      router.replace('/');
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      onLogin(token);
+    }
+  }, [token]);
+
+  return <div>Loading...</div>;
+}
+
+export default DevLocalLogin;
--- apps/web/src/app/dev/local/page.tsx
+++ apps/web/src/app/dev/local/page.tsx
@@ -1,33 +1,14 @@
-'use client';
+import dynamic from 'next/dynamic';
 
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+const LazyLoginProgress = dynamic(() => import('./LoginProgress'), { ssr: false });
 
-import { useLogin } from '@/store/user';
-
-function DevLocalLoginPage() {
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token');
-
-  const router = useRouter();
-  const { login } = useLogin();
-
-  const onLogin = async (token: string) => {
-    try {
-      await login(token);
-      router.replace('/mypage');
-    } catch (error) {
-      router.replace('/');
-    }
-  };
-
-  useEffect(() => {
-    if (token) {
-      onLogin(token);
-    }
-  }, [token]);
-
-  return <div>Loading...</div>;
+function DevLocalLoginPage({ searchParams }: { searchParams: URLSearchParams }) {
+  console.log('searchParams: ', searchParams);
+  return (
+    <>
+      <LazyLoginProgress />
+    </>
+  );
 }
 
 export default DevLocalLoginPage;
