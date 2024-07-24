1b1c87059393f44c7bd0c4df27901a4c9e1d6b89
sumi.byun <selina2000@naver.com>
2024-07-24 16:26:07 +0900

    feat: Landing 페이지에 login button 추가 (#136)
    * feat: choose section desktop/mobile 추가
    
    * feat: login button 추가
    
    * fix: eslint build error fix

--- apps/web/src/app/landing/MainSection/MainSection.tsx
+++ apps/web/src/app/landing/MainSection/MainSection.tsx
@@ -2,6 +2,7 @@ import { Button } from '@gitanimals/ui-panda';
 
 import * as styles from './MainSection.style';
 import MainSlider from './MainSlider';
+import LoginButton from '@/components/LoginButton';
 
 function MainSection() {
   return (
@@ -11,12 +12,14 @@ function MainSection() {
         You can acquire and grow pets through GitHub activities. Choose from over 50 different pets and raise them.
       </p>
       {/* TODO: button 반응형 처리 */}
-      <Button size="m" className="mobile">
-        Have Pet
-      </Button>
-      <Button size="l" className="desktop">
-        Have Pet
-      </Button>
+      <LoginButton>
+        <Button size="m" className="mobile">
+          Have Pet
+        </Button>
+        <Button size="l" className="desktop">
+          Have Pet
+        </Button>
+      </LoginButton>
       <picture className={styles.bg}>
         <source srcSet="/main/section1_bg-mobile.webp" media="(max-width: 768px)" type="image/webp" />
         <img src="/main/section1_bg-pc.webp" alt="section background" />
--- apps/web/src/components/LoginButton.tsx
+++ apps/web/src/components/LoginButton.tsx
@@ -1,5 +1,5 @@
+'use client';
 import type { PropsWithChildren } from 'react';
-import React from 'react';
 
 import { GITHUB_OAUTH_REQUEST_URL } from '@/constants/oauth';
 import { useUser } from '@/store/user';
@@ -7,15 +7,11 @@ import { useUser } from '@/store/user';
 interface LoginButtonProps {}
 
 function LoginButton({ children }: PropsWithChildren<LoginButtonProps>) {
-  const { isLogin } = useUser();
+  const { isLogin } = useUser(); // TODO: useUser 제거
 
   if (isLogin) return children;
 
-  return (
-    <a href={GITHUB_OAUTH_REQUEST_URL} onClick={(e) => e.stopPropagation()}>
-      {children}
-    </a>
-  );
+  return <a href={GITHUB_OAUTH_REQUEST_URL}>{children}</a>;
 }
 
 export default LoginButton;
