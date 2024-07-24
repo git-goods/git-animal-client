057762a985145e6fa5459f429105e0c087cd5288
sumi.byun <selina2000@naver.com>
2024-07-24 21:27:27 +0900

    Google Sheet를 이용한 Logging (#137)
    * feat: google sheet 라이브러리 연결
    
    * feat: google sheet에 send log 값 보내기
    
    * feat: log util 추가
    
    * refactor: google sheet 로직 정리
    
    * feat: log에 설명 추가
    
    * chore: pakcakge lock solve
    
    * fix: any type to object

---
+++ apps/web/src/app/api/googleSheet/route.ts
@@ -0,0 +1,85 @@
+import { JWT } from 'google-auth-library';
+import type { GoogleSpreadsheetWorksheet } from 'google-spreadsheet';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+const privateKey = process.env.GOOGLE_PRIVATE_KEY;
+const accountEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
+const documentID = process.env.GOOGLE_LOG_DOCUMENT_ID;
+
+type GoogleSheetType = 'general';
+
+interface GoogleSheetValue {
+  headerValues: string[];
+  title: string;
+}
+
+const GOOGLE_SHEET: Record<GoogleSheetType, GoogleSheetValue> = {
+  general: {
+    headerValues: ['createdAt', 'data', 'description'],
+    title: 'general',
+  },
+};
+
+async function loadGoogleDocument() {
+  try {
+    const formattedKey = privateKey?.replace(/\\n/g, '\n');
+
+    const serviceAccountAuth = new JWT({
+      key: formattedKey,
+      email: accountEmail,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
+    if (!documentID) throw new Error('documentID is not defined');
+    if (!serviceAccountAuth) throw new Error('serviceAccountAuth is not defined');
+
+    const doc = new GoogleSpreadsheet(documentID, serviceAccountAuth);
+    await doc.loadInfo();
+
+    return doc;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const loadGoogleSheet = async (doc: GoogleSpreadsheet, type: GoogleSheetType): Promise<GoogleSpreadsheetWorksheet> => {
+  let sheet = doc.sheetsByTitle[type];
+
+  if (!sheet) {
+    sheet = await doc.addSheet(GOOGLE_SHEET[type]);
+  }
+
+  return sheet;
+};
+
+export async function POST(request: Request) {
+  const res = await request.json();
+  const type = 'general';
+
+  try {
+    const doc = await loadGoogleDocument();
+    if (!doc) return Response.json({ success: false, error: 'google sheet를 찾지 못했습니다.' });
+
+    const sheet = await loadGoogleSheet(doc, type);
+
+    const { description, ...data } = res;
+
+    await sheet.addRow({
+      createdAt: getCurrentKoreaTime(),
+      data: JSON.stringify(data),
+      description,
+    });
+
+    return Response.json({ success: true });
+  } catch (error: any) {
+    return new Response(error.message, { status: 400 });
+  }
+}
+
+const getCurrentKoreaTime = () => {
+  const now = new Date();
+  const utc = now.getTime() + now.getTimezoneOffset() * 60 * 1000;
+  const koreaTimeDiff = 9 * 60 * 60 * 1000;
+
+  return new Date(utc + koreaTimeDiff).toLocaleString();
+};
