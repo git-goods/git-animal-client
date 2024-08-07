15870bc05d7ba60f8f0c5637b8f533e809d8840d
hyesungoh <haesungoh414@gmail.com>
2024-08-08 08:34:24 +0900

    chore: update github url and open link with blank

--- apps/web/src/constants/outlink.ts
+++ apps/web/src/constants/outlink.ts
@@ -1,4 +1,4 @@
 export const STATIC_IMAGE_URL = 'https://static.gitanimals.org/personas';
-export const GIT_ANIMALS_MAIN_URL = 'https://github.com/devxb/gitanimals';
+export const GIT_ANIMALS_MAIN_URL = 'https://github.com/git-goods/gitanimals';
 
 export const SUMI_GITHUB_URL = 'https://github.com/sumi-0011';
