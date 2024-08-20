8c5c0e54fd40d8fbf3d8b46207e74b54187b05c5
sumi-0011 <selina2000@naver.com>
2024-08-20 15:41:43 +0900

    feat: login button 자동 클릭

--- apps/web/src/app/auth/LoginButton.tsx
+++ apps/web/src/app/auth/LoginButton.tsx
@@ -3,9 +3,18 @@ import React from 'react';
 import { signIn } from 'next-auth/react';
 
 function LoginButton({ token }: { token: string }) {
+  const ref = React.useRef<HTMLButtonElement>(null);
+
+  React.useEffect(() => {
+    if (ref.current) {
+      ref.current.click();
+    }
+  }, [ref.current]);
+
   return (
     <div>
       <button
+        ref={ref}
         onClick={() =>
           signIn('credentials', {
             token: token,
@@ -15,9 +24,6 @@ function LoginButton({ token }: { token: string }) {
       >
         LogIn
       </button>
-      {/* <button className="rounded-xl border bg-red-300 px-12 py-4" onClick={() => signOut()}>
-        Log Out
-      </button> */}
     </div>
   );
 }
--- apps/web/src/app/auth/page.tsx
+++ apps/web/src/app/auth/page.tsx
@@ -1,36 +1,39 @@
-'use client';
-
-import { useSearchParams } from 'next/navigation';
+import { center } from '_panda/patterns';
 
 import LoginButton from './LoginButton';
 
-function JWTPage(
-  {
-    // searchParams,
-  }: {
-    // searchParams: {
-    //   jwt: string;
-    // };
-  },
-) {
-  const searchParams = useSearchParams();
-  const jwtToken = searchParams?.get('jwt') || '';
+function JWTPage({
+  searchParams,
+}: {
+  searchParams: {
+    jwt: string;
+  };
+}) {
+  const jwtToken = searchParams.jwt;
   const token = jwtToken.split(' ')[1];
 
   return (
-    <>
-      Login Loading...
-      <LoginButton token={token} />
-    </>
+    <div className={loadingContainerStyle}>
+      Loading....
+      <div style={{ visibility: 'hidden' }}>
+        <LoginButton token={token} />
+      </div>
+    </div>
   );
 }
 
 export default JWTPage;
 
-// const Container = styled.div`
-//   height: 100vh;
-//   width: 100vw;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `;
+const loadingContainerStyle = center({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(255, 255, 255, 0.8)',
+  zIndex: 9999,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '36px',
+});
