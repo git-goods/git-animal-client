63f3da9a2d9cb959ee08bb34535cd28744001fa5
sumi-0011 <selina2000@naver.com>
2024-08-28 00:47:13 +0900

    fix: main product 상품 안나오는 문제 수정

--- packages/api/src/auction/getTotalProductCount.ts
+++ packages/api/src/auction/getTotalProductCount.ts
@@ -2,11 +2,11 @@ import z from 'zod';
 import { safeGet } from '../_instance/safe';
 
 export const GetTotalProductCountResponseSchema = z.object({
-  count: z.number(),
+  count: z.string(),
 });
 
 export type GetTotalProductCountResponse = z.infer<typeof GetTotalProductCountResponseSchema>;
 
 export const getTotalProductCount = (): Promise<GetTotalProductCountResponse> => {
-  return safeGet(GetTotalProductCountResponseSchema)('/products/statistics/total');
+  return safeGet(GetTotalProductCountResponseSchema)('/auctions/statistics/products/total');
 };
