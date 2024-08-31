521f6aedf2a25c7198164d591e84f8db241783ca
sumi-0011 <selina2000@naver.com>
2024-09-01 02:34:27 +0900

    refactor: icon packages로 분리

--- apps/web/src/app/shop/Search/index.tsx
+++ apps/web/src/app/shop/Search/index.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import { css, cx } from '_panda/css';
 import { center, flex } from '_panda/patterns';
+import { CloseIcon, SearchIcon } from '@gitanimals/ui-icon';
 
 import { useGetPersonaTypes } from '@/apis/auctions/useGetPersonaTypes';
 import DottedThreeBox from '@/components/DottedBox/DottedThreeBox';
@@ -38,7 +39,7 @@ function Search(props: SearchProps) {
               <div className={selectPersonaStyle}>
                 Selected Persona : {props.selected}
                 <button onClick={() => onClick()}>
-                  <CloseIcon />
+                  <CloseIcon color="#878787" />
                 </button>
               </div>
             )}
@@ -61,14 +62,6 @@ function Search(props: SearchProps) {
 }
 
 export default Search;
-
-function SearchIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
-      <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
-    </svg>
-  );
-}
 
 const buttonWrapperStyle = center({
   width: '100%',
@@ -124,10 +117,10 @@ const personaItemStyle = css({
   },
 });
 
-function CloseIcon() {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#878787">
-      <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
-    </svg>
-  );
-}
+// function CloseIcon() {
+//   return (
+//     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#878787">
+//       <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+//     </svg>
+//   );
+// }
--- packages/ui/icon/src/index.ts
+++ packages/ui/icon/src/index.ts
@@ -2,3 +2,5 @@ export { GithubIcon } from './icons/GithubIcon';
 export { LinkMonoIcon } from './icons/LinkMonoIcon';
 export { BehanceIcon } from './icons/BehanceIcon';
 export { XIcon } from './icons/XIcon';
+export { SearchIcon } from './icons/SearchIcon';
+export { CloseIcon } from './icons/CloseIcon';
