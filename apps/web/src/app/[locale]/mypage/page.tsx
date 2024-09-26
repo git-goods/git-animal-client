237b9950ba26e3b8b118244d36d8dcd4281d3f48
sumi-0011 <selina2000@naver.com>
2024-09-26 14:48:54 +0900

    feat: tab item 추가

--- apps/web/src/app/[locale]/mypage/page.tsx
+++ apps/web/src/app/[locale]/mypage/page.tsx
@@ -1,26 +1,71 @@
-// import React from 'react';
 import Image from 'next/image';
-import { css } from '_panda/css';
-import { grid } from '_panda/patterns';
+import { css, cx } from '_panda/css';
+import { flex, grid } from '_panda/patterns';
+import { updateUrlSearchParams } from '@gitanimals/util-common';
 
 import GNB from '@/components/GNB/GNB';
+import { Link } from '@/i18n/routing';
 
 import { ProfileSection } from './ProfileSection';
 
-function Mypage() {
+function Mypage({
+  searchParams,
+}: {
+  searchParams: {
+    type?: 'farm-type' | '1-type';
+  };
+
+  pathname: string;
+}) {
+  const selectedType = searchParams?.type ?? '1-type';
+
   return (
     <div className={containerStyle}>
       <GNB />
       <Image src="/mypage/bg-cloud.webp" alt="bg" width={2400} height={1367} className={bgStyle} />
       <main className={mainStyle}>
         <ProfileSection />
-        {/* <RightSection /> */}
+        <section>
+          <div className={tabListStyle}>
+            <Link href={`/mypage?${updateUrlSearchParams(searchParams, 'type', '1-type')}`}>
+              <button className={cx('tab-item', selectedType === '1-type' && 'selected')}>1 Type</button>
+            </Link>
+            <Link href={`/mypage?${updateUrlSearchParams(searchParams, 'type', 'farm-type')}`}>
+              <button className={cx('tab-item', selectedType === 'farm-type' && 'selected')}>Farm Type</button>
+            </Link>
+          </div>
+        </section>
       </main>
     </div>
   );
 }
 
 export default Mypage;
+
+const tabListStyle = flex({
+  gap: 12,
+  '& .tab-item': {
+    textStyle: 'glyph28.bold',
+    borderRadius: 12,
+    padding: 12,
+    textAlign: 'center',
+    border: '1.5px solid',
+    height: 58,
+    color: 'white.white_25',
+    backgroundColor: 'white.white_10',
+    borderColor: 'white.white_10',
+    '&.selected': {
+      color: 'white.white_100',
+      backgroundColor: 'white.white_25',
+      borderColor: 'white.white_50',
+    },
+    _hover: {
+      color: 'white.white_75',
+      backgroundColor: 'white.white_25',
+      borderColor: 'white.white_50',
+    },
+  },
+});
 
 const containerStyle = css({
   minHeight: '100vh',
