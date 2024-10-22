f3fcdc6487bd4ec67ad24cf1c1669c2733063618
sumi-0011 <selina2000@naver.com>
2024-10-22 11:03:04 +0900

    fix: admin build error fix

--- apps/admin/app/routes/shop.drop-pet.tsx
+++ apps/admin/app/routes/shop.drop-pet.tsx
@@ -1,19 +1,19 @@
 import ProductDataTable from '@/components/shop/drop-pet/ProductTable';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
-import { RenderPersonaItem, getAllMyPets } from '@gitanimals/api';
+import { Persona, getAllMyPersonas } from '@gitanimals/api';
 import { LoaderFunction, json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { Flex } from '_panda/jsx';
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const { personas } = await getAllMyPets({ username: 'git-good-w' });
+  const { personas } = await getAllMyPersonas('git-good-w');
   return json({ personas });
 };
 
 function ShopDropPetPage() {
   const data = useLoaderData<typeof loader>();
 
-  const personas: RenderPersonaItem[] = data.personas;
+  const personas: Persona[] = data.personas;
 
   return (
     <Flex p={4}>
