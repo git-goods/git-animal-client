e59660006d38485b84071a59cb09bfb636a1cef0
sumi.byun <selina2000@naver.com>
2024-11-08 11:57:08 +0900

    할로윈 이벤트 끝 (#231)

--- apps/web/src/app/[locale]/event/[eventCode]/page.tsx
+++ apps/web/src/app/[locale]/event/[eventCode]/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
+import { useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
 import { css } from '_panda/css';
 import { flex } from '_panda/patterns';
-
-import { sendMessageToErrorChannel } from '@/apis/slack/sendMessage';
 
 import { CardList, MobileCardList } from './CardList';
 import { Draw } from './Draw';
@@ -23,38 +22,73 @@ async function HalloweenEventPage({ params }: { params: Params }) {
   const isAfterHalloween = now.getMonth() === 10 && now.getDate() > 7;
   if (isAfterHalloween) {
     // NOTE: 11월이 되면 그냥 이벤트 종료하는 식으로 하드 코딩
-    sendMessageToErrorChannel('할로윈 기간 지낫음 하드코딩한거 바꾸삼');
-    notFound();
+    // sendMessageToErrorChannel('할로윈 기간 지낫음 하드코딩한거 바꾸삼');
+    // notFound();
   }
 
   const t = await getTranslations('Event.Halloween');
 
   return (
-    <div className={containerStyle}>
-      <Image
-        src="/event/halloween/halloween-title.svg"
-        alt="gitanimals halloween event"
-        width={1357}
-        height={199}
-        objectFit="contain"
-        className={logoImageStyle}
-        draggable={false}
-      />
+    <>
+      <div className={containerStyle}>
+        <Image
+          src="/event/halloween/halloween-title.svg"
+          alt="gitanimals halloween event"
+          width={1357}
+          height={199}
+          objectFit="contain"
+          className={logoImageStyle}
+          draggable={false}
+        />
 
-      <h2 className={descriptionStyle}>{t('description')}</h2>
-      <div className={showMobile}>
-        <MobileCardList />
-      </div>
-      <div className={showDesktop}>
-        <CardList />
-      </div>
+        <h2 className={descriptionStyle}>{t('description')}</h2>
+        <div className={showMobile}>
+          <MobileCardList />
+        </div>
+        <div className={showDesktop}>
+          <CardList />
+        </div>
 
-      <Draw />
-    </div>
+        <Draw />
+      </div>
+      {isAfterHalloween && <EventEndOverlay />}
+    </>
   );
 }
 
 export default HalloweenEventPage;
+
+function EventEndOverlay() {
+  const t = useTranslations('Event');
+
+  return (
+    <div
+      className={css({
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 1000,
+        overflow: 'hidden',
+      })}
+    >
+      <h1
+        className={css({
+          color: 'white',
+          textAlign: 'center',
+          textStyle: 'glyph32.bold',
+        })}
+      >
+        {t('event-end')}
+      </h1>
+    </div>
+  );
+}
 
 const showDesktop = css({
   display: 'block',
