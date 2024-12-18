fb0e249514e49edf3ba710ca54ad266cf9ceb258
hyesung oh <haesungoh414@gmail.com>
2024-12-18 16:21:14 +0900

    fix: show more pet button to anchor (#250)

--- packages/ui/panda/src/components/Button/Button.tsx
+++ packages/ui/panda/src/components/Button/Button.tsx
@@ -2,3 +2,4 @@ import { styled } from '_panda/jsx';
 import { buttonStyle } from './cva';
 
 export const Button = styled('button', buttonStyle);
+export const AnchorButton = styled('a', buttonStyle);
