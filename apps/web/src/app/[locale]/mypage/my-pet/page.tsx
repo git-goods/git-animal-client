c301265820072dbcfcc3487970f25d3fbc0fa2e6
sumi-0011 <selina2000@naver.com>
2024-12-23 12:53:40 +0900

    refactor: my pet의 table 교체

--- apps/web/src/app/[locale]/mypage/my-pet/page.tsx
+++ apps/web/src/app/[locale]/mypage/my-pet/page.tsx
@@ -4,10 +4,11 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { useTranslations } from 'next-intl';
 import { css, cx } from '_panda/css';
+import { Flex } from '_panda/jsx';
 import { flex } from '_panda/patterns';
 import { dropPet, type Persona } from '@gitanimals/api';
 import { userQueries } from '@gitanimals/react-query';
-import { Button } from '@gitanimals/ui-panda';
+import { Button, Table } from '@gitanimals/ui-panda';
 import { snakeToTitleCase } from '@gitanimals/util-common';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'sonner';
@@ -113,35 +114,46 @@ function SelectedPetTable({ currentPersona, reset }: SelectedPetTableProps) {
   };
 
   return (
-    <div className={tableCss}>
-      <div className={theadCss}>
-        <span>{t('pet')}</span>
-        <span>{t('name')}</span>
-        <span>{t('grade')}</span>
-        <span>{t('level')}</span>
-        <span></span>
-      </div>
-
-      <div className={cx(rowStyle, 'row')}>
-        {currentPersona && (
-          <>
-            <div>
+    <Table className={tableCss}>
+      <Table.Header>
+        <Table.Row>
+          <Table.Head>{t('pet')}</Table.Head>
+          <Table.Head>{t('name')}</Table.Head>
+          <Table.Head>{t('grade')}</Table.Head>
+          <Table.Head>{t('level')}</Table.Head>
+          <Table.Head></Table.Head>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {currentPersona ? (
+          <Table.Row>
+            <Table.Cell paddingLeft={6}>
               <Image src={getPersonaImage(currentPersona.type)} alt={currentPersona.type} width={60} height={67} />
-            </div>
-            <div>{snakeToTitleCase(currentPersona.type)}</div>
-            <div>{ANIMAL_TIER_TEXT_MAP[getAnimalTierInfo(Number(currentPersona.dropRate.replace('%', '')))]}</div>
-            <div>{currentPersona.level}</div>
-            <div className={flex({ gap: '8px' })}>
+            </Table.Cell>
+            <Table.Cell>{snakeToTitleCase(currentPersona.type)}</Table.Cell>
+            <Table.Cell>
+              {ANIMAL_TIER_TEXT_MAP[getAnimalTierInfo(Number(currentPersona.dropRate.replace('%', '')))]}
+            </Table.Cell>
+            <Table.Cell>{currentPersona.level}</Table.Cell>
+            <Table.Cell className={flex({ gap: '8px', minH: '80px', alignItems: 'center' })}>
               <Button variant="secondary" onClick={onSellClick}>
                 100P {t('sell')}
               </Button>
               <Button variant="secondary" onClick={() => setIsMergeOpen(true)}>
                 {t('merge')}
               </Button>
-            </div>
-          </>
+            </Table.Cell>
+          </Table.Row>
+        ) : (
+          <Table.Row>
+            <Table.Cell colSpan={5}>
+              <Flex alignItems="center" justifyContent="center" w="full" h="full">
+                Please select a pet.
+              </Flex>
+            </Table.Cell>
+          </Table.Row>
         )}
-      </div>
+      </Table.Body>
 
       {currentPersona && (
         <MergePersona
@@ -151,65 +163,11 @@ function SelectedPetTable({ currentPersona, reset }: SelectedPetTableProps) {
           targetPersona={currentPersona}
         />
       )}
-    </div>
+    </Table>
   );
 }
 
 const tableCss = css({
   width: '100%',
   marginBottom: '32px',
-});
-
-const theadCss = css({
-  display: 'grid',
-  gridTemplateColumns: '1fr 2.5fr 1fr 1fr 5.7fr',
-  gap: '16px',
-  padding: '4px 32px',
-  borderRadius: '12px',
-  backgroundColor: 'white_50',
-  alignItems: 'center',
-
-  height: '46px',
-  color: 'white_100',
-
-  '& > span:nth-child(1)': {
-    textAlign: 'center',
-  },
-
-  marginBottom: '4px',
-
-  _mobile: {
-    fontSize: '16px',
-  },
-});
-
-const rowStyle = css({
-  width: '100%',
-  height: '80px',
-  backgroundColor: 'white_10',
-  borderRadius: '12px',
-
-  display: 'grid',
-  gridTemplateColumns: '1fr 2.5fr 1fr 1fr 5.7fr',
-  alignItems: 'center',
-  padding: '0 32px',
-  gap: '16px',
-
-  textStyle: 'glyph20.regular',
-  color: 'white.white_100',
-
-  '& button': {
-    color: 'black.black',
-    width: '100%',
-    paddingX: '6px',
-  },
-
-  '& *': {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-  },
-
-  _mobile: {
-    fontSize: '16px',
-  },
 });
