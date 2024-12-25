8624688f8ac337ea8ed8b83869d83104ef2a5144
sumi-0011 <selina2000@naver.com>
2024-12-25 19:42:52 +0900

    feat: 길드 레이아웃 추가

--- apps/web/src/app/[locale]/guild/layout.tsx
+++ apps/web/src/app/[locale]/guild/layout.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import { css } from '_panda/css';
 
 import GNB from '@/components/GNB/GNB';
@@ -7,11 +6,7 @@ export default function GuildLayout({ children }: { children: React.ReactNode })
   return (
     <>
       <GNB />
-      <div className={containerStyle}>
-        {children}
-
-        <Image src="/guild/init-bg-bottom.png" className="bg-bottom" alt="init-bg-bottom" width={3600} height={228} />
-      </div>
+      <div className={containerStyle}>{children}</div>
     </>
   );
 }
@@ -24,15 +19,6 @@ const containerStyle = css({
   overflow: 'hidden',
   position: 'relative',
 
-  '& .bg-bottom': {
-    position: 'absolute',
-    width: '100vw',
-    bottom: 0,
-    left: '50%',
-    transform: 'translateX(-50%)',
-    height: '228px',
-    objectFit: 'cover',
-  },
   _mobile: {
     minHeight: '100vh ',
   },
--- apps/web/src/app/[locale]/guild/page.tsx
+++ apps/web/src/app/[locale]/guild/page.tsx
@@ -1,3 +1,20 @@
+import Image from 'next/image';
+import { css } from '_panda/css';
+
 export default function GuildPage() {
-  return <div>GuildPage</div>;
+  return (
+    <div>
+      <Image src="/guild/init-bg-bottom.png" className={bottomBgStyle} alt="init-bg-bottom" width={3600} height={228} />
+    </div>
+  );
 }
+
+const bottomBgStyle = css({
+  position: 'absolute',
+  width: '100vw',
+  bottom: 0,
+  left: '50%',
+  transform: 'translateX(-50%)',
+  height: '228px',
+  objectFit: 'cover',
+});
