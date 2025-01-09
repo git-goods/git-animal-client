315b3fe1514e72b309ea8a75971c4af77a98fa7d
sumi-0011 <selina2000@naver.com>
2025-01-09 14:21:33 +0900

    feat: 길드 설정 메뉴 접근 제어

---
+++ apps/web/src/app/[locale]/guild/[id]/MoreMenu.tsx
@@ -0,0 +1,48 @@
+import { getGuildById, getUser } from '@gitanimals/api';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@gitanimals/ui-panda';
+import { CatIcon, EllipsisVerticalIcon, LinkIcon, SettingsIcon, UsersRoundIcon } from 'lucide-react';
+
+import { Link } from '@/i18n/routing';
+
+/**
+ * 더보기 메뉴
+ * 현재는 모두 길드장만 볼 수 있음
+ */
+export async function MoreMenu({ guildId }: { guildId: string }) {
+  const data = await getGuildById({ guildId: guildId });
+  const user = await getUser();
+
+  const isLeader = data.leader.userId === user.id;
+
+  if (!isLeader) return null;
+
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <EllipsisVerticalIcon size={24} color="#FFFFFFBF" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start" sideOffset={10} alignOffset={-4}>
+          <Link href={`/guild/${guildId}/setting`}>
+            <DropdownMenuItem>
+              <SettingsIcon color="#FFFFFF80" size={18} />
+              Guild setting
+            </DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem>
+            <CatIcon color="#FFFFFF80" size={18} />
+            Edit profile pet
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <UsersRoundIcon color="#FFFFFF80" size={18} />
+            Manage members
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <LinkIcon color="#FFFFFF80" size={18} />
+            Send invite message
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
