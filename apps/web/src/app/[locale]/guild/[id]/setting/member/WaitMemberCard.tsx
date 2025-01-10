f205dbfab27a7766750823dc6dfbd5ffb7577c59
sumi-0011 <selina2000@naver.com>
2025-01-10 23:11:01 +0900

    feat: 가입 수락, 제거

--- apps/web/src/app/[locale]/guild/[id]/setting/member/WaitMemberCard.tsx
+++ apps/web/src/app/[locale]/guild/[id]/setting/member/WaitMemberCard.tsx
@@ -3,18 +3,26 @@
 import { Flex } from '_panda/jsx';
 import { acceptJoinGuild, denyJoinGuild, type GuildMember } from '@gitanimals/api';
 import { Button } from '@gitanimals/ui-panda';
+import { toast } from 'sonner';
 
+import { useRouter } from '@/i18n/routing';
 import { getPersonaImage } from '@/utils/image';
 
 import { BannerGuildMember } from './BannerGuildMember';
 
 export function WaitMemberCard({ member, guildId }: { member: GuildMember; guildId: string }) {
+  const router = useRouter();
+
   const acceptMember = async () => {
-    await acceptJoinGuild({ guildId: guildId, userId: member.id });
+    await acceptJoinGuild({ guildId: guildId, userId: member.userId });
+    toast.success('Accepted member');
+    router.refresh();
   };
 
   const denyMember = async () => {
-    await denyJoinGuild({ guildId: guildId, userId: member.id });
+    await denyJoinGuild({ guildId: guildId, userId: member.userId });
+    toast.success('Denied member');
+    router.refresh();
   };
 
   return (
--- apps/web/src/app/[locale]/guild/[id]/setting/member/page.tsx
+++ apps/web/src/app/[locale]/guild/[id]/setting/member/page.tsx
@@ -1,57 +1,40 @@
 import { css, cx } from '_panda/css';
 import { Flex } from '_panda/jsx';
-import { getGuildById, kickMemberFromGuild } from '@gitanimals/api';
-import { Button, dialogTitleStyle } from '@gitanimals/ui-panda';
+import { getGuildById } from '@gitanimals/api';
+import { dialogTitleStyle } from '@gitanimals/ui-panda';
 
-import { getPersonaImage } from '@/utils/image';
-
-import { BannerGuildMember } from './BannerGuildMember';
+import { MemberCard } from './MemberCard';
 import { WaitMemberCard } from './WaitMemberCard';
 
 export default async function GuildMemberSetting({ params }: { params: { id: string } }) {
   const guild = await getGuildById({ guildId: params.id });
 
-  const removeMember = async (formData: FormData) => {
-    'use server';
-    const guildId = formData.get('guildId') as string;
-    const userId = formData.get('userId') as string;
-    await kickMemberFromGuild({ guildId, userId });
-  };
-
   return (
     <div>
       <h2 className={cx(dialogTitleStyle, css({ mb: '40px' }))}>Manage members</h2>
 
-      <h3 className={cx(subHeadingStyle, css({ mb: '24px' }))}>Request</h3>
-      {guild?.waitMembers.length === 0 ? (
-        <p className={css({ textAlign: 'center', pt: '40px', color: 'white.white_50' })}>No members</p>
-      ) : (
-        <Flex gap="1" flexWrap="wrap">
-          {guild.waitMembers.map((member) => (
-            <WaitMemberCard member={member} guildId={guild.id} key={member.id} />
-          ))}
-        </Flex>
+      {guild?.waitMembers.length === 0 ? null : (
+        <>
+          <h3 className={subHeadingStyle}>Request</h3>
+          <Flex gap="1" flexWrap="wrap" mb="24px">
+            {guild.waitMembers.map((member) => (
+              <WaitMemberCard member={member} guildId={guild.id} key={member.id} />
+            ))}
+          </Flex>
+        </>
       )}
 
-      <h3 className={subHeadingStyle}>Members</h3>
       {guild?.members.length === 0 ? (
-        <p className={css({ textAlign: 'center', pt: '40px', color: 'white.white_50' })}>No members</p>
+        <p className={css({ textAlign: 'center', py: '40px', color: 'white.white_50' })}>No members</p>
       ) : (
-        <Flex gap="1" flexWrap="wrap">
-          {guild.members.map((member) => (
-            <form key={member.id} action={removeMember}>
-              <input type="hidden" name="guildId" value={guild.id} />
-              <input type="hidden" name="userId" value={member.id} />
-              <BannerGuildMember
-                key={member.id}
-                image={getPersonaImage(member.personaType)}
-                name={member.name}
-                count={member.contributions}
-                bottomElement={<Button>Remove</Button>}
-              />
-            </form>
-          ))}
-        </Flex>
+        <>
+          <h3 className={subHeadingStyle}>Members</h3>
+          <Flex gap="1" flexWrap="wrap">
+            {guild.members.map((member) => (
+              <MemberCard member={member} key={member.id} guildId={guild.id} />
+            ))}
+          </Flex>
+        </>
       )}
     </div>
   );
