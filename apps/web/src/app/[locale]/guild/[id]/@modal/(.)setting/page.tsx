2a5ed017085c9a3c93001df8ecfae763dc3f3b39
sumi.byun <selina2000@naver.com>
2025-01-11 15:48:42 +0900

    Intercepting용 Dialog 추가 및 적용 (#272)

---
+++ apps/web/src/app/[locale]/guild/[id]/@modal/(.)setting/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from 'next/navigation';
+import { checkIsLeader, getGuildBackgrounds, getGuildById, getGuildIcons } from '@gitanimals/api';
+import { DialogTitle } from '@gitanimals/ui-panda';
+
+import { InterceptingDialog } from '@/components/InterceptingDialog';
+
+import { GuildSetting } from '../../setting/GuildSetting';
+
+export default async function GuildSettingModal({ params }: { params: { id: string } }) {
+  const isLeader = await checkIsLeader(params.id);
+
+  const icons = await getGuildIcons();
+  const backgrounds = await getGuildBackgrounds();
+  const data = await getGuildById({ guildId: params.id });
+
+  if (!isLeader) {
+    redirect(`/guild/${params.id}`);
+  }
+
+  return (
+    <InterceptingDialog>
+      <DialogTitle>Guild Setting</DialogTitle>
+      <GuildSetting icons={icons} backgrounds={backgrounds} guildId={params.id} initialData={data} />
+    </InterceptingDialog>
+  );
+}
