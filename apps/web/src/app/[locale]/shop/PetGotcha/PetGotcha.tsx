6ff7b6be78c055053ced622a466c14240b331dca
박승훈 <tmdgns1126@naver.com>
2025-02-13 23:43:28 +0900

    style: mobile version of pet gotcha section of shop page

--- apps/web/src/app/[locale]/shop/PetGotcha/PetGotcha.tsx
+++ apps/web/src/app/[locale]/shop/PetGotcha/PetGotcha.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import { useState } from 'react';
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+import { css } from '_panda/css';
+import useIsMobile from '@gitanimals/react/src/hooks/useIsMobile/useIsMobile';
 import { userQueries } from '@gitanimals/react-query';
 import { Button } from '@gitanimals/ui-panda';
 import { wrap } from '@suspensive/react';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { css } from '_panda/css';
-import { useTranslations } from 'next-intl';
-import Image from 'next/image';
-import { useState } from 'react';
+
+import { getResponsiveImage } from '@/utils/image';
 
 import OnePet from './OnePet';
 import { TenPet } from './TenPet';
@@ -16,9 +19,12 @@ export function PetGotcha() {
   const t = useTranslations('Gotcha');
   const [openModal, setOpenModal] = useState<'one-pet' | 'ten-pet' | 'ratio-chart' | null>(null);
 
+  const isMobile = useIsMobile();
+  const bgSrc = getResponsiveImage('/shop/pet-gotcha-bg', isMobile);
+
   return (
     <div className={containerStyle}>
-      <Image src="/shop/pet-gotcha-bg.webp" alt="pet-gotcha-bg" width={1920} height={1226} className={bgStyle} />
+      <Image src={bgSrc} alt="pet-gotcha-bg" width={1920} height={1226} className={bgStyle} />
       <h1 className={headingStyle}>Pet Gotcha</h1>
       <p className={descStyle}>{t('pet-gotcha-desc')}</p>
 
@@ -52,15 +58,17 @@ const ButtonWrapper = wrap
   })
   .on(({ onClickOnePet, onClickTenPet }: ButtonWrapperProps) => {
     const { data } = useSuspenseQuery(userQueries.userOptions());
+    const isMobile = useIsMobile();
+    const buttonSize = isMobile ? 's' : 'l';
 
     const points = Number(data.points);
 
     return (
       <div className={buttonContainerStyle}>
-        <Button size="l" onClick={onClickOnePet} disabled={points < 1_000}>
+        <Button size={buttonSize} onClick={onClickOnePet} disabled={points < 1_000}>
           1 Pet / 1,000 P
         </Button>
-        <Button size="l" onClick={onClickTenPet} disabled={points < 10_000}>
+        <Button size={buttonSize} onClick={onClickTenPet} disabled={points < 10_000}>
           10 Pets / 10,000 P
         </Button>
       </div>
@@ -74,6 +82,10 @@ const containerStyle = css({
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
+
+  _mobile: {
+    padding: '100px 0',
+  },
 });
 
 const bgStyle = css({
@@ -89,18 +101,33 @@ const bgStyle = css({
 const headingStyle = css({
   textStyle: 'glyph82.bold',
   color: 'white',
+
+  _mobile: {
+    textStyle: 'glyph40.bold',
+  },
 });
 
 const descStyle = css({
   textStyle: 'glyph24.regular',
   color: 'white',
   marginTop: '16px',
+
+  _mobile: {
+    marginTop: '8px',
+    textStyle: 'glyph16.regular',
+  },
 });
 
 const buttonContainerStyle = css({
   display: 'flex',
   gap: '16px',
   marginTop: '80px',
+
+  _mobile: {
+    gap: '8px',
+    paddingInline: '16px',
+    marginTop: '20px',
+  },
 });
 
 const petContainerStyle = css({
@@ -109,5 +136,11 @@ const petContainerStyle = css({
   '& img': {
     objectFit: 'contain',
     margin: '0 auto',
+  },
+
+  _mobile: {
+    width: '100%',
+    paddingInline: '16px',
+    marginTop: '80px',
   },
 });
