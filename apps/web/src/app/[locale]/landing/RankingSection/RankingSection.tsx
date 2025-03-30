79e79d5289c7f0f6dd4ed1377eadc221eca881f7
sumi.byun <selina2000@naver.com>
2025-03-30 18:48:21 +0900

    Ranking View 추가 (#300)

--- apps/web/src/app/[locale]/landing/RankingSection/RankingSection.tsx
+++ apps/web/src/app/[locale]/landing/RankingSection/RankingSection.tsx
@@ -1,12 +1,38 @@
-import { css } from '_panda/css';
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { css, cx } from '_panda/css';
+import { rankQueries } from '@gitanimals/react-query';
+import { useQuery } from '@tanstack/react-query';
+
+import { Link } from '@/i18n/routing';
 
 import GameConsole from './GameConsole/GameConsole';
+import { RankingTable } from './RankingTable';
+import { TopPodium } from './TopPodium';
 
 export default function RankingSection() {
+  const searchParams = useSearchParams();
+  const selectedTab = searchParams.get('ranking') ?? 'people';
+
+  const { data: ranks } = useQuery({
+    ...rankQueries.getRanksOptions({
+      rank: 1,
+      size: 8,
+      type: selectedTab === 'people' ? 'WEEKLY_USER_CONTRIBUTIONS' : 'WEEKLY_GUILD_CONTRIBUTIONS',
+    }),
+  });
+
   return (
     <div className={containerStyle}>
       <GameConsole>
-        <div className={screenContentStyle}>안녕하세요!</div>
+        {ranks && (
+          <div className={screenContentStyle}>
+            <RankingTab selectedTab={selectedTab} />
+            <TopPodium ranks={ranks.slice(0, 3)} />
+            <RankingTable ranks={ranks.slice(3)} />
+          </div>
+        )}
       </GameConsole>
     </div>
   );
@@ -20,18 +46,53 @@ const containerStyle = css({
   backgroundColor: '#111827',
   padding: '130px',
   overflow: 'hidden',
+
+  _mobile: {
+    display: 'none',
+  },
 });
 
 const screenContentStyle = css({
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  padding: '60px 130px 0',
   color: '#4ADE80',
-  fontFamily: 'monospace',
-  fontSize: '36px',
-  '@media (min-width: 640px)': {
-    fontSize: '48px',
-  },
+  fontFamily: 'token(fonts.dnf)',
+});
+
+function RankingTab({ selectedTab }: { selectedTab: string }) {
+  return (
+    <div className={tabsStyle}>
+      <Link
+        href="/?ranking=people"
+        shallow
+        scroll={false}
+        className={cx(tabStyle, selectedTab === 'people' && selectedTabStyle)}
+      >
+        People
+      </Link>
+      <Link
+        href="/?ranking=guild"
+        shallow
+        scroll={false}
+        className={cx(tabStyle, selectedTab === 'guild' && selectedTabStyle)}
+      >
+        Guild
+      </Link>
+    </div>
+  );
+}
+
+const tabsStyle = css({
+  display: 'flex',
+  justifyContent: 'center',
+  mb: '60px',
+});
+
+const tabStyle = css({
+  textStyle: 'glyph18.bold',
+  color: 'white.white_50',
+  p: '4px 10px',
+});
+
+const selectedTabStyle = css({
+  color: 'white.white_100',
 });
