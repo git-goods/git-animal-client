7efc25dfcb1946f0575b591fadccd478f54d42d1
tmdgns1126 <tmdgns1126@naver.com>
2025-04-13 14:45:58 +0900

    chore: 퀴즈 풀기 상태 조회 api 추가

--- packages/api/src/quiz/index.ts
+++ packages/api/src/quiz/index.ts
@@ -2,5 +2,6 @@ export * from './answerQuiz';
 export * from './createQuiz';
 export * from './createQuizContext';
 export * from './getQuiz';
+export * from './getQuizToday';
 export * from './getRoundResult';
 export * from './stopContext';
