b66babc98f5788c8233386f28d174686e953b8d8
sumi-0011 <selina2000@naver.com>
2025-06-27 20:38:32 +0900

    fix: sub layout error fix

--- apps/webview/src/components/Layout/SubLayout.tsx
+++ apps/webview/src/components/Layout/SubLayout.tsx
@@ -1,42 +1,47 @@
 'use client';
 
-import { css } from '_panda/css';
+import { css, cx } from '_panda/css';
 import { ChevronLeftIcon } from 'lucide-react';
 
 import { useRouter } from '@/i18n/routing';
+import { containerStyle } from '@/utils/style';
 
 export const SubLayout = ({ children, title }: { children: React.ReactNode; title: string }) => {
   const router = useRouter();
 
   return (
     <div
-      className={css({
-        backgroundColor: 'black',
-        h: '100vh',
-        maxW: '560px',
-        mx: 'auto',
-        color: 'white',
-      })}
+      className={cx(
+        containerStyle,
+        css({
+          h: '100vh',
+          backgroundColor: 'black',
+          color: 'white',
+        }),
+      )}
     >
       <div
-        className={css({
-          position: 'fixed',
-          height: 'var(--mobile-sub-header-height)',
-          width: '100%',
-          zIndex: 101,
-          top: 0,
-          left: 0,
-          right: 0,
-          minHeight: '44px',
-          display: 'grid',
-          gridTemplateColumns: '24px 1fr 24px',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          px: 4,
-        })}
+        className={cx(
+          containerStyle,
+          css({
+            position: 'fixed',
+            height: 'var(--mobile-sub-header-height)',
+            width: '100%',
+            zIndex: 101,
+            top: 0,
+            left: 0,
+            right: 0,
+            minHeight: '44px',
+            display: 'grid',
+            gridTemplateColumns: '24px 1fr 24px',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            px: 4,
+          }),
+        )}
       >
         <button onClick={() => router.back()}>
-          <ChevronLeftIcon size={24} />
+          <ChevronLeftIcon size={24} color="white" />
         </button>
         <h1
           className={css({
@@ -69,28 +74,34 @@ export function BottomButton({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <div
-        className={css({
-          position: 'fixed',
-          bottom: '-34px',
-          zIndex: 99,
-          left: 0,
-          pointerEvents: 'none',
-          right: 0,
-          bg: 'linear-gradient(180deg, rgba(24, 26, 29, 0) 0%, #181A1D 50%)',
-          height: '160px',
-        })}
+        className={cx(
+          containerStyle,
+          css({
+            position: 'fixed',
+            bottom: '-34px',
+            zIndex: 99,
+            left: 0,
+            pointerEvents: 'none',
+            right: 0,
+            bg: 'linear-gradient(180deg, rgba(24, 26, 29, 0) 0%, #181A1D 50%)',
+            height: '160px',
+          }),
+        )}
       />
       <div
-        className={css({
-          position: 'fixed',
-          bottom: 0,
-          width: '100%',
-          zIndex: 100,
-          px: 4,
-          py: 2,
-          left: 0,
-          right: 0,
-        })}
+        className={cx(
+          containerStyle,
+          css({
+            position: 'fixed',
+            bottom: 0,
+            width: '100%',
+            zIndex: 100,
+            px: 4,
+            py: 2,
+            left: 0,
+            right: 0,
+          }),
+        )}
       >
         {children}
       </div>
---
+++ apps/webview/src/utils/style.ts
@@ -0,0 +1,6 @@
+import { css } from '_panda/css';
+
+export const containerStyle = css({
+  maxW: '560px',
+  mx: 'auto',
+});
