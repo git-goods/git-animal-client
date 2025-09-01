68d43f13ad0dea3846be7e02a63e62a318ea98d4
sumi-exem <sumi@ex-em.com>
2025-09-01 10:21:44 +0900

    chore: app project 제거

--- apps/app/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-import { View, Text } from 'react-native';
-import { useEffect } from 'react';
-import { router } from 'expo-router';
-
-export default function IndexScreen() {
-  return <Redirect href="/auth/login" />;
-}
