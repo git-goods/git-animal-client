ef2f1cc8b92c1ee7a68d79d3eb45177cdc990dd8
tmdgns1126 <tmdgns1126@naver.com>
2025-09-28 17:24:51 +0900

    feat: mobile layout logo + description + backgroundImage 구현

--- apps/web/src/app/[locale]/game/quiz/layout.tsx
+++ apps/web/src/app/[locale]/game/quiz/layout.tsx
@@ -5,7 +5,7 @@ import { MobileLayout } from '@/app/[locale]/game/quiz/_components/MobileLayout'
 export default function QuizLayout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <MobileLayout>{children}</MobileLayout>
+      <MobileLayout background={{ url: '/assets/game/quiz/quiz-bg.webp', position: 'bottom' }}>{children}</MobileLayout>
     </Suspense>
   );
 }
