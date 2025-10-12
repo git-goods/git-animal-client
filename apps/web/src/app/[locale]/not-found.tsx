694f80e007da364d242ae514fb0ed8d9d36d3fb8
박승훈 <tmdgns1126@naver.com>
2025-10-12 17:18:49 +0900

    다국어 관련 설정(next-intl, i18n-ally) (#345)

--- apps/web/src/app/[locale]/not-found.tsx
+++ apps/web/src/app/[locale]/not-found.tsx
@@ -1,8 +1,51 @@
 'use client';
 
-import Error from 'next/error';
+import { useEffect } from 'react';
+import { useTranslations } from 'next-intl';
+import { css } from '_panda/css';
+
+import { sendMessageToErrorChannel } from '@/apis/slack/sendMessage';
+import { ErrorPage } from '@/components/Error/ErrorPage';
+import { isDev } from '@/constants/env';
+import { GITHUB_ISSUE_URL } from '@/constants/outlink';
+import { usePathname, useRouter } from '@/i18n/routing';
 
 export default function NotFound() {
-  console.log('Not Found -------------------------------');
-  return <Error statusCode={404} title="Not Found" />;
+  const pathname = usePathname();
+  const t = useTranslations('NotFound');
+
+  useEffect(() => {
+    if (isDev) return;
+
+    sendMessageToErrorChannel(`<!here>
+🌌 Not Found 🌌
+Path: ${pathname}
+`);
+  }, [pathname]);
+
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.push('/');
+  };
+
+  return (
+    <ErrorPage
+      heading={t('title')}
+      paragraph={
+        <div className={css({ textAlign: 'center' })}>
+          {t('main-description')}
+          <br />
+          <br />
+          <div className={css({ color: 'gray.500' })}>
+            {t('description-line-1')} [<a href={GITHUB_ISSUE_URL}>GitHub</a>]
+            <br />
+            {t('description-line-2')}
+          </div>
+        </div>
+      }
+      onClickButton={onClickButton}
+      buttonText={t('home-button-text')}
+    />
+  );
 }
