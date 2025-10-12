cbde517943f8d606cf497b84faa30c6cc73ba9c1
tmdgns1126 <tmdgns1126@naver.com>
2025-10-12 17:13:02 +0900

    fix: not-found html 구조 오류 수정

--- apps/web/src/app/[locale]/not-found.tsx
+++ apps/web/src/app/[locale]/not-found.tsx
@@ -1,17 +1,16 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { useLocale, useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl';
 import { css } from '_panda/css';
 
 import { sendMessageToErrorChannel } from '@/apis/slack/sendMessage';
 import { ErrorPage } from '@/components/Error/ErrorPage';
 import { isDev } from '@/constants/env';
 import { GITHUB_ISSUE_URL } from '@/constants/outlink';
+import { usePathname, useRouter } from '@/i18n/routing';
 
 export default function NotFound() {
-  const locale = useLocale();
   const pathname = usePathname();
   const t = useTranslations('NotFound');
 
@@ -31,26 +30,22 @@ Path: ${pathname}
   };
 
   return (
-    <html lang={locale}>
-      <body>
-        <ErrorPage
-          heading={t('title')}
-          paragraph={
-            <div className={css({ textAlign: 'center' })}>
-              {t('main-description')}
-              <br />
-              <br />
-              <div className={css({ color: 'gray.500' })}>
-                {t('description-line-1')} [<a href={GITHUB_ISSUE_URL}>GitHub</a>]
-                <br />
-                {t('description-line-2')}
-              </div>
-            </div>
-          }
-          onClickButton={onClickButton}
-          buttonText={t('home-button-text')}
-        />
-      </body>
-    </html>
+    <ErrorPage
+      heading={t('title')}
+      paragraph={
+        <div className={css({ textAlign: 'center' })}>
+          {t('main-description')}
+          <br />
+          <br />
+          <div className={css({ color: 'gray.500' })}>
+            {t('description-line-1')} [<a href={GITHUB_ISSUE_URL}>GitHub</a>]
+            <br />
+            {t('description-line-2')}
+          </div>
+        </div>
+      }
+      onClickButton={onClickButton}
+      buttonText={t('home-button-text')}
+    />
   );
 }
