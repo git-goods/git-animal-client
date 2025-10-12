d79364b55b5c37da6bd1335c0a56c47580b9ccc3
sumi <sumi@sumiui-MacBookAir.local>
2025-10-12 22:20:42 +0900

    feat: pathname header에 값 저장

--- apps/web/src/app/[locale]/laboratory/layout.tsx
+++ apps/web/src/app/[locale]/laboratory/layout.tsx
@@ -1,8 +1,23 @@
+import { headers } from 'next/headers';
 import { css } from '_panda/css';
 
-export default function LaboratoryLayout({ children }: { children: React.ReactNode }) {
-  return <div className={containerStyle}>{children}</div>;
+export default async function LaboratoryLayout({ children }: { children: React.ReactNode }) {
+  const headersList = headers();
+  const pathname = headersList.get('x-pathname') || '';
+  console.log('pathname', pathname);
+
+  return (
+    <div className={containerStyle}>
+      {children}
+      <footer>
+        <p>
+          실험실 기능이 마음에 드셨나요? 정식 출시를 원하시면 피드백을 남겨주세요! <button>[피드백 남기기]</button>
+        </p>
+      </footer>
+    </div>
+  );
 }
+
 const containerStyle = css({
   minHeight: '100vh',
   height: 'fit-content',
