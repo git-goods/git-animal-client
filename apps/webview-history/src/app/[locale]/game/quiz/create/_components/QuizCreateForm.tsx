011e2080a395e9125f0fed6b37e0a8956e46b770
박승훈 <tmdgns1126@naver.com>
2025-10-23 14:26:04 +0900

    Quiz UX 개선 및 현재 locale 확인 로직 추가 (#346)

--- apps/webview-history/src/app/[locale]/game/quiz/create/_components/QuizCreateForm.tsx
+++ apps/webview-history/src/app/[locale]/game/quiz/create/_components/QuizCreateForm.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import { useLocale, useTranslations } from 'next-intl';
 import { css } from '_panda/css';
 import { createQuiz } from '@gitanimals/api';
@@ -12,7 +11,7 @@ import Tabs from '@/components/Tabs/Tabs';
 import TabsList from '@/components/Tabs/TabsList';
 import TabsTrigger from '@/components/Tabs/TabsTrigger';
 import useTabs from '@/components/Tabs/useTabs';
-import type { Locale } from '@/i18n/routing';
+import { type Locale, useRouter } from '@/i18n/routing';
 
 import type { QuizCategory, QuizLevel } from '../../_constants/quiz.constants';
 import { QUIZ_ANSWER, QUIZ_CATEGORY, QUIZ_LEVEL, QUIZ_RESULT } from '../../_constants/quiz.constants';
