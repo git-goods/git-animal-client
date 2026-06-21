444ce913ad222f14ed845a7ecb592c8c2c8bc055
박승훈 <tmdgns1126@naver.com>
2026-06-21 17:42:13 +0900

    feat: ui-tailwind에 NoRatingCard 추가
    event(christmas/halloween) 가 쓰는 ui-panda NoRatingCard 를 panda 원본 1:1 로 이식(리프 div+img).
    container 265x328, bg/thumbnail absolute, info absolute bottom flex, typeText glyph24-bold+leading-[40px],
    ratingText glyph22-regular+leading-[40px]. 전역 클래스 훅(animal-card-*) 유지. snakeToTitleCase 동봉(panda 동일).

--- packages/ui/tailwind/src/components/card/index.ts
+++ packages/ui/tailwind/src/components/card/index.ts
@@ -1,4 +1,5 @@
 export { GameCard } from './GameCard';
 export { CardBack } from './CardBack';
+export { NoRatingCard, snakeToTitleCase } from './NoRatingCard';
 export { CARD_INFO, ANIMAL_CARD_IMAGE_BASE_URL } from './constants';
 export type { CardTierType } from './constants';
